b3a83a7c284b3ea885bfee1cfefb06cbba49d6c0
Thomas Grainger <tom@procensus.com>
2017-06-06 11:27:20 +0100

    test on typed_ast

--- tests/test_unparse.py
+++ tests/test_unparse.py
@@ -6,15 +6,29 @@ else:
     import unittest
 
 import astunparse
-from tests.common import AstunparseCommonTestCase
+from tests.common import ASTTestCase, AST3TestCase, AST2TestCase
 
-class UnparseTestCase(AstunparseCommonTestCase, unittest.TestCase):
+class UnparseTestCase(object):
 
     def assertASTEqual(self, ast1, ast2):
         self.assertEqual(ast.dump(ast1), ast.dump(ast2))
 
     def check_roundtrip(self, code1, filename="internal", mode="exec"):
-        ast1 = compile(str(code1), filename, mode, ast.PyCF_ONLY_AST)
+        ast1 = self.compile(str(code1), filename, mode)
         code2 = astunparse.unparse(ast1)
-        ast2 = compile(code2, filename, mode, ast.PyCF_ONLY_AST)
+        ast2 = self.compile(code2, filename, mode)
         self.assertASTEqual(ast1, ast2)
+
+
+class ASTUnparseTestCase(UnparseTestCase, ASTTestCase, unittest.TestCase):
+    pass
+
+
+if AST3TestCase:
+    class AST3UnparseTestCase(UnparseTestCase, AST3TestCase, unittest.TestCase):
+        pass
+
+
+if AST2TestCase:
+    class AST3UnparseTestCase(UnparseTestCase, AST2TestCase, unittest.TestCase):
+        pass
